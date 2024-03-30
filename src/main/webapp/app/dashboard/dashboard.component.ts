22cafa84f5a207cae9ada99a9108a15ed11746cc
krokodil1113 <krokodil111384@gmail.com>
2024-03-30 14:18:00 +0100

    Added display for quizzes by category

--- src/main/webapp/app/dashboard/dashboard.component.ts
+++ src/main/webapp/app/dashboard/dashboard.component.ts
@@ -1,29 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { QuizService } from '../entities/quiz/service/quiz.service';
+import { CategoryService } from '../entities/category/service/category.service';
 
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { IQuiz } from 'app/entities/quiz/quiz.model';
+import { ICategory } from 'app/entities/category/category.model';
 
 @Component({
   selector: 'jhi-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   quizzes: IQuiz[] = [];
+  categories: ICategory[] = [];
   selectedCategoryId = 1; // Example category ID
 
-  constructor(private quizService: QuizService) {}
+  constructor(
+    private quizService: QuizService,
+    private categoryService: CategoryService,
+  ) {}
 
   ngOnInit(): void {
-    this.loadQuizzesByCategory(this.selectedCategoryId);
+    this.loadCategories();
+    // this.loadQuizzesByCategory(this.selectedCategoryId);
+  }
+
+  loadCategories(): void {
+    this.categoryService.query().subscribe({
+      next: response => {
+        if (response.body) {
+          this.categories = response.body;
+        } else {
+          console.warn('No categories returned from server');
+        }
+      },
+      error: error => console.error('Error fetching categories:', error),
+    });
   }
 
   loadQuizzesByCategory(categoryId: number): void {
+    console.log('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa', categoryId);
+    if (!categoryId) {
+      this.quizzes = [];
+      return;
+    }
+    this.selectedCategoryId = categoryId;
+    this.quizService
+      .getQuizzesByCategory(categoryId)
+      .pipe(
+        catchError(error => {
+          console.error('Error fetching quizzes by category:', error);
+          return of([]); // Providing a fallback value in case of an error
+        }),
+      )
+      .subscribe(data => (this.quizzes = data));
+  }
+
+  /* loadQuizzesByCategory(categoryId: number): void {
     this.quizService
       .getQuizzesByCategory(categoryId)
       .pipe(
@@ -36,5 +75,5 @@ export class DashboardComponent {
       .subscribe(data => {
         this.quizzes = data;
       });
-  }
+  } */
 }
